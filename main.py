bb51ff423cc66efc2950a370cfd927f1345e394c
Pavel <165821654+MortiZum@users.noreply.github.com>
2024-04-09 20:26:56 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,17 @@
-from tkinter import *
- 
-root = Tk()
-root.title("Игра лабиринт")
-root.geometry("800x500")
- 
-label = Label(text="")
-label.pack()
- 
-root.mainloop()
+
+pygame.init()
+
+screen = pygame.display.set_mode((800, 600))
+
+pygame.display.set_caption("Лабиринт")
+background = transform.scale(image.load('kosmos.jpg'), (800, 600))
+while True:
+
+    for e in pygame.event.get():
+
+        if e.type == pygame.QUIT:
+
+            raise SystemExit("QUIT")
+
+
+    pygame.display.update()
